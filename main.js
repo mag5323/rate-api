3fd23bef4628bb118dbe5fea5ac2b70e5f526f25
mag5323 <mag5323@gmail.com>
2015-01-23 16:45:37 +0800

    add yql query

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+  var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url="rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm" and xpath="//a[contains(@id, \'DownloadCsv\')]"&format=json';
+
   function ajax(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
