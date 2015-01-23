50d70cc3c6c4819a5f11f70011e93b428b5c9729
mag5323 <mag5323@gmail.com>
2015-01-23 16:45:16 +0800

    add ajax function

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 window.onload = function() {
+  function ajax(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        callback.apply(xhr);
+      }
+    }
+    xhr.send();
+  }
 };
