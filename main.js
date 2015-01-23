7dbe7160b5792cfd2ffa6211d2b21f9b343041bf
mag5323 <mag5323@gmail.com>
2015-01-23 19:00:26 +0800

    fix coding style

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-  var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url="rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm" and xpath="//a[contains(@id, \'DownloadCsv\')]"&format=json';
+  var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url=' +
+            '"rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm" and xpath="//a[contains(@id, \'DownloadCsv\')]"&format=json';
 
   function ajax(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -16,7 +17,8 @@ window.onload = function() {
     var result = JSON.parse(this.responseText).query.results.a.href;
     var datePosition = result.indexOf('date');
     var time = result.substr(datePosition + 5, 19);
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url="rate.bot.com.tw/Pages/UIP003/Download.ashx%3Flang=zh-TW%26fileType=1%26date=' + time + '"&format=json';
+    var url = 'http://query.yahooapis.com/v1/public/yql?q=select * from html where url=' +
+              '"rate.bot.com.tw/Pages/UIP003/Download.ashx%3Flang=zh-TW%26fileType=1%26date=' + time + '"&format=json';
 
     ajax(url, function() {
       var row = JSON.parse(this.responseText).query.results.body.p.split(",");
