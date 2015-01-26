9f22f482c88f35967edc81ccb2ac05be7ce5b26a
mag5323 <mag5323@gmail.com>
2015-01-26 14:51:59 +0800

    get latest update time

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var request = require('request')
 
 request('http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm', function(err, res, body) {
+  var datetimePosition = body.indexOf('date')
+  var datetime = body.substr(datetimePosition + 5, 19)
 })
