b6aa39fc0494783e3e29dd6aa81b75917722d100
mag5323 <mag5323@gmail.com>
2015-01-26 15:05:47 +0800

    deal with rate csv file

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ var request = require('request')
 request('http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm', function(err, res, body) {
   var datetimePosition = body.indexOf('date')
   var datetime = body.substr(datetimePosition + 5, 19)
+
+  var url = 'http://rate.bot.com.tw/Pages/UIP003/Download.ashx?lang=zh-TW&fileType=1&date=' + datetime;
+  request(url, function(err, res, body) {
+    var result = body.split(',').map(function(column) {
+      return column.trim()
+    })
+  })
 })
